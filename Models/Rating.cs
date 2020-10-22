200286d9be71b924c176772a68138080631ca2c8
Sergio Silva <sergiobtos@hotmail.com>
2020-10-22 12:46:48 -0400

    3

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -8,6 +8,7 @@ namespace WebApplicationBeanstalk.Models
     public class Rating
     {
         public User User { get; set; }
+        
         public string Comment { get; set; }
         public int Rate { get; set; }
         public DateTime Date { get; set; }
